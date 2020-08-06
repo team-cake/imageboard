a4472a6e05b60e9f50d8d1c7ecc9990364eed671
team-cake <eka.cheuk@gmail.com>
2020-08-06 10:49:47 +0200

    Added offset and limit with pagination

--- routers/image.js
+++ routers/image.js
@@ -4,9 +4,19 @@ const router = new Router()
 const Image = require('../models').image
 
 // get all images
-router.get('/', async (req, res) => {
-	const images = await Image.findAll()
-	res.send(images)
+// router.get('/', async (req, res) => {
+// 	const images = await Image.findAll()
+// 	res.send(images)
+// })
+
+//get all images with limit, offset and findAndCountAll
+router.get('/', (req, res, next) => {
+	const limit = Math.min(req.query.limit || 25, 500)
+	const offset = req.query.offset || 0
+
+	Image.findAndCountAll({ limit, offset })
+		.then((result) => res.send({ images: result.rows, total: result.count }))
+		.catch((error) => next(error))
 })
 
 // post new image
